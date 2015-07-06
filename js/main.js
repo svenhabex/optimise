d604501fac9448d66f87f5c4093475852ba3bbef
svenhabex <sven.habex@hotmail.com>
2015-07-06 15:03:59 +0200

    header

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        var scroll = $(this).scrollTop();
+
+        if(scroll > 70){
+            $('.navbar-default').css('background-color', 'rgba(60,60,60,1)')
+        }else if(scroll <= 70){
+            $('.navbar-default').css('background-color', 'rgba(0,0,0,0)')
+        }
+    });
+});
